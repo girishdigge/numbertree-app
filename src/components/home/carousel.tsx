41ce3e0b56f463057639851cbeb28edba294e266
girishdigge <girishdigge@gmail.com>
2024-03-08 00:05:16 +0530

    homepage 15

--- src/components/home/carousel.tsx
+++ src/components/home/carousel.tsx
@@ -84,21 +84,21 @@ const Carousel = () => {
               <source src={slide.video} type='video/mp4' />
             </video>
 
-            <div className='absolute inset-0  flex items-center md:pl-16 md:pt-20 pt-2 mb-10  bg-black bg-opacity-30'>
+            <div className='absolute inset-0  flex items-center md:pl-16 md:pt-20 pt-2 mb-11  bg-black bg-opacity-30'>
               <div className='bg-opacity-50 md:w-4/5 h-full p-5'>
                 <div className='text-white'>
                   {/* <div className='text-xl'>{slide.heading}</div> */}
-                  <h2 className='md:text-xl font-bold text-sm ml-4 '>
+                  <h2 className='md:text-2xl font-bold text-sm ml-4 '>
                     {slide.weAre}
                   </h2>
                   {/* <Separator
                     orientation='vertical'
                     className='bg-num-orange w-1 h-10'
                   /> */}
-                  <h2 className='md:text-6xl pl-2  text-sm  border-l-4 border-num-orange'>
+                  <h2 className='md:text-7xl pl-2  text-sm  border-l-4 border-num-orange'>
                     {slide.title}
                   </h2>
-                  <p className='md:text-xl ml-4 font-sans md:pt-5 md:pb-2 text-xs md:w-5/12'>
+                  <p className='md:text-2xl ml-4 font-sans md:pt-5 md:pb-2 text-xs md:w-5/12'>
                     {slide.content}
                   </p>
                   <Button className='w-40 ml-4 bg-num-indigo md:mt-5 mt-2 border-0 hover:bg-num-orange'>
@@ -111,7 +111,7 @@ const Carousel = () => {
                 </div> */}
               </div>
             </div>
-            <div className='z-100 text-3xl bg-num-indigo pb-2 text-num-orange flex justify-end'>
+            <div className='z-100 text-3xl font-semibold bg-num-indigo pb-2 pr-2 text-num-orange flex justify-end'>
               {`#Empowering Tomorrow's infrastructure`}
             </div>
           </div>
