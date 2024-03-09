fc37e4f69ff44efc5de1253f133715ee63ebcd6e
girishdigge <girishdigge@gmail.com>
2024-03-09 19:12:03 +0530

    error resolved

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,13 +5,3 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-export const connectToDB = async () => {
-  const connection: any = {};
-  try {
-    if (connection.isConnected) return;
-    const db: any = await mongoose.connect(process.env.MONGO!);
-    connection.isConnected = db.connections[0].readyState;
-  } catch (error: any) {
-    throw new Error(error);
-  }
-};
